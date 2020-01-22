3afc514fcdc33f1bc11d601cba216f19e0d390a2
WenDev <wendev1024@icloud.com>
2020-01-22 23:05:57 +0800

    修正了不能渲染表格的bug

--- src/main/java/site/wendev/website/util/MarkdownUtils.java
+++ src/main/java/site/wendev/website/util/MarkdownUtils.java
@@ -3,33 +3,37 @@ package site.wendev.website.util;
 import org.commonmark.ext.gfm.tables.TableBlock;
 import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.node.Code;
+import org.commonmark.node.IndentedCodeBlock;
 import org.commonmark.node.Node;
 import org.commonmark.node.Paragraph;
 import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.AttributeProvider;
-import org.commonmark.renderer.html.AttributeProviderContext;
-import org.commonmark.renderer.html.AttributeProviderFactory;
-import org.commonmark.renderer.html.HtmlRenderer;
+import org.commonmark.renderer.NodeRenderer;
+import org.commonmark.renderer.html.*;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 
 public class MarkdownUtils {
     /**
      * Markdown格式的文档转换成HTML
      */
-    public static String markdown2Html(String markdown) {
+    public static String markdown2Html1(String markdown) {
         var parser = Parser.builder().build();
         var render = HtmlRenderer.builder().build();
         var document = parser.parse(markdown);
         return render.render(document);
     }
 
-    public static String extensions(String markdown) {
+    public static String markdown2Html(String markdown) {
         var tableExtension = Arrays.asList(TablesExtension.create());
         var parser = Parser.builder().extensions(tableExtension).build();
         var document = parser.parse(markdown);
-        var render = HtmlRenderer.builder().extensions(tableExtension)
+        var render = HtmlRenderer.builder().nodeRendererFactory(new HtmlNodeRendererFactory() {
+            @Override
+            public NodeRenderer create(HtmlNodeRendererContext context) {
+                return new IndentedCodeBlockNodeRenderer(context);
+            }
+        })
+                .extensions(tableExtension)
                 .attributeProviderFactory(new AttributeProviderFactory() {
                     @Override
                     public AttributeProvider create(AttributeProviderContext attributeProviderContext) {
@@ -45,10 +49,33 @@ public class MarkdownUtils {
             if (node instanceof TableBlock) {
                 map.put("class", "table table-striped");
             }
+        }
+    }
 
-            if (node instanceof Code) {
-                map.put("class", "prettyprint linenums");
-            }
+    static class IndentedCodeBlockNodeRenderer implements NodeRenderer {
+
+        private final HtmlWriter html;
+
+        IndentedCodeBlockNodeRenderer(HtmlNodeRendererContext context) {
+            this.html = context.getWriter();
+        }
+
+        @Override
+        public Set<Class<? extends Node>> getNodeTypes() {
+            // Return the node types we want to use this renderer for.
+            return Collections.<Class<? extends Node>>singleton(IndentedCodeBlock.class);
+        }
+
+        @Override
+        public void render(Node node) {
+            var attrs = new HashMap<String, String>();
+            attrs.put("class", "prettyprint linenums");
+            IndentedCodeBlock codeBlock = (IndentedCodeBlock) node;
+            html.line();
+            html.tag("pre", attrs);
+            html.text(codeBlock.getLiteral());
+            html.tag("/pre");
+            html.line();
         }
     }
 }
